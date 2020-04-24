e6220b91894acb04bc49e6f52fd51cae2b600b40
EC2 Default User <ec2-user@ip-172-31-80-30.ec2.internal>
2020-04-24 15:17:40 +0000

    Make sure to source the environment before launching the database

--- scripts/run-db.sh
+++ scripts/run-db.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+source ./prod_env
 docker container stop postgresben || true && docker container rm postgresben || true
 docker run --name postgresben -e POSTGRES_PASSWORD=$FLASK_DATABASE_PASS \
     -v $POSTGRES_PERSISTANT_DATA_DIRECTORY:/var/lib/postgresql/data \
